cced79504c1fea3689190a6cc518b710871a4785
RafaelAPNascimento <rfx.go.on@gmail.com>
2022-02-04 10:45:34 -0300

    bypass tls

--- src/test/java/tls/TLSUtil.java
+++ src/test/java/tls/TLSUtil.java
@@ -74,10 +74,16 @@ public class TLSUtil {
         SSLContext sslcontext = SSLContext.getInstance("TLS");
 
         sslcontext.init(null, new TrustManager[]{new X509TrustManager() {
-            public void checkClientTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {}
-            public void checkServerTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {}
-            public X509Certificate[] getAcceptedIssuers() { return new X509Certificate[0]; }
-        }}, new java.security.SecureRandom());
+
+                public void checkClientTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {}
+
+                public void checkServerTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {}
+
+                public X509Certificate[] getAcceptedIssuers() { return new X509Certificate[0]; }
+
+            }},
+            new java.security.SecureRandom()
+        );
 
         return sslcontext;
     }
